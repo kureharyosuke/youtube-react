925b58f710d7799c508eba4cb72f66956d78f33d
root <root@DESKTOP-BFBU1HU.localdomain>
2020-08-10 13:50:13 +0900

    #1.1 MainContainer & VideoCard Build

--- src/MainContainer.js
+++ src/MainContainer.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import './MainContainer.css';
+import VideoCard from './VideoCard';
 
 const MainContainer = () => {
     return (
         <div className='mainContainer'>
             <h1>MainContainer</h1>
-        <VideoCard/>
+            <VideoCard/>
+            <VideoCard/>
+            <VideoCard/>
+            <VideoCard/>
+            <VideoCard/>
+            <VideoCard/>
         </div>
     )
 }
